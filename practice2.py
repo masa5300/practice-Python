1b9d35003f772266ae41f1a280a7ec92597e95b3
imamura masamune <kurobasu1432@gmail.com>
2024-04-24 00:36:27 +0900

    Day2-1

--- practice2.py
+++ practice2.py
@@ -1,1 +1,17 @@
-print("wow")
+menu = ['salad','soup','steak']
+menu.append('cake')
+print(menu[-1])
+print(menu[1])
+menu = ['salad','soup','steak','cake']
+print(menu[1])
+menu[-1] = 'pudding'
+print(menu)
+menu = ['salad','soup','steak','cake']
+print(menu[1:3])
+menu2 = menu[:-1]
+print(menu2)
+menu = ['salad','soup','steak']
+menu.append('cake')
+print(menu[-1])
+menu.remove('soup')
+print(menu[1:])
