d9a48a4c094ff1d2ee296d31f494c63c7ce45fec
imamura masamune <kurobasu1432@gmail.com>
2024-04-24 12:54:32 +0900

    5

--- practice3.py
+++ practice3.py
@@ -12,3 +12,10 @@ if age < 20:
     print('NO Drink')
 else:
     print('Enjoy')
+print(True and False)
+print(True or False)
+print(not True)
+print(not False)
+BMI = 20
+if BMI >= 18 and BMI < 25:
+    print('nomal weight')
