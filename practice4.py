1d4cf0bdace86bb64421ad1424e8566675eb3960
imamura masamune <kurobasu1432@gmail.com>
2024-04-25 20:34:16 +0900

    3

--- practice4.py
+++ practice4.py
@@ -6,3 +6,16 @@ def greet():
 
 def wow():
     print("びっくり")
+    
+def greet(name):
+    print(name+'さん、こんにちは')
+    
+    greet('松尾')
+    
+    greet('岩澤')
+
+def greet(name):
+    string = name + 'さん、こんにちは'
+    return string
+string = greet('松尾')
+print(string)
